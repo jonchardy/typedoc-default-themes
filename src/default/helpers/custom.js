430824f75fd29bea77a781054f6990a12802ee40
Jon Hardy <jonathan.c.hardy@gmail.com>
2017-04-26 15:12:34 -0400

    Constant tag updates

--- src/default/helpers/custom.js
+++ src/default/helpers/custom.js
@@ -1,18 +1,20 @@
 var helpers = {
+	// check if this declaration contains a particular tag
 	containsTag: function(value, declaration, options) {
-		// fallback...
 		var array = [];
-		if(declaration && declaration.signatures){
-			declaration.signatures.forEach(function(s){
-				if(s.comment && s.comment.tags){
+		if (declaration && declaration.signatures) {
+			// pull the tags from the signatures if they exist
+			declaration.signatures.forEach(function(s) {
+				if (s.comment && s.comment.tags) {
 					array = array.concat(s.comment.tags);
 				}
 			});
-
-			//if(array) console.log("contains", value, array);
+		} else if (declaration && declaration.comment && declaration.comment.tags) {
+			// otherwise pull from the declaration itself
+			array = array.concat(declaration.comment.tags);
 		}
 
-		array = array.map(function(t){
+		array = array.map(function(t) {
 			if (t) {
 				return t.tagName;
 			}
